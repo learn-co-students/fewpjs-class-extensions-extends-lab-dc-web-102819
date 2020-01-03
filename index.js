6a1ff2712908a0cdcd72de52c6201b80d84f4e02
Kyle <kylepcole@gmail.com>
2020-01-03 15:53:07 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+class Polygon {
+	constructor(sides) {
+		this.sides = sides
+	}
+
+	get countSides() {
+		return this.sides.length
+	}
+
+	get perimeter() {
+		let perimeter = 0
+		return this.sides.reduce((perimeter, side) => perimeter += side)
+		// return this.sides.forEach(side => perimiter += side)
+	}
+}
+
+class Triangle extends Polygon {
+
+	get isValid() {
+	// isValid() {
+		let valid = true
+		console.log('initial valid = ' + valid)
+		//invalid if more or less than 3 sides
+		if (this.countSides !== 3) {
+			valid = false
+			console.log('does not have three sides; valid = ' + valid)
+			// return valid
+		} 
+
+		//if three sideed validation passed, compare side lengths
+		if (valid) {
+			if ((this.sides[0] + this.sides[1] < this.sides[2]) ||
+			 (this.sides[1] + this.sides[2] < this.sides[0]) ||
+			 (this.sides[2] + this.sides[0] < this.sides[1])) {
+				valid = false
+				console.log('is not a triangle; valid is set to ' + valid)
+			}
+		}
+
+		//return true/false
+		console.log('valid output = ' + valid)
+		return valid
+	}
+}
+
+class Square extends Polygon {
+
+	get isValid() {
+		if ((this.sides[0] === this.sides[1]) && (this.sides[0] === this.sides[2]) && (this.sides[0] === this.sides[3])) {
+			return true
+		} else return false
+	}
+
+	get area() {
+		return this.sides[0]**2
+	}
+}
