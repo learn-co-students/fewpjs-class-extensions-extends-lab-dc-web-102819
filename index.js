7374daa09cb8ddd41a859d538066e9d296620876
jdromero88 <github email address>
2020-01-04 17:19:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Polygon {
+  constructor(array) {
+    this.array = array
+  }
+
+  get countSides(){
+    return this.array.length
+  }
+  get perimeter(){
+    let perimeter = 0
+    this.array.forEach(function(lado) {
+      perimeter += lado
+    })
+    return perimeter
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid(){
+    if (this.array.length === 3) {
+      let valid = new Set(this.array).size === 1
+      return valid
+    } else {
+      return false
+    }
+  }
+}
+
+class Square extends Polygon{
+  get isValid(){
+    if(this.array.length === 4){
+      let valid = new Set(this.array).size === 1
+      return valid
+    }
+    else {
+      return false
+    }
+  }
+  get area(){
+    return this.array[0] **2
+  }
+}
